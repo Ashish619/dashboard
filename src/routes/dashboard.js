aabc9a53db981d8b913c0ae9ea3b60c594b854d9
ASHISH <ashish.anand619@gmail.com>
2018-02-22 02:06:57 +0530

    dashboard added donout chart

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -10,6 +10,7 @@ import ChartComposed from '../components/dashboard/composedChart'
 import styles from './dashboard.less'
 import { color } from '../utils'
 import { getTotalCount, getCount } from '../services/dashboard.js'
+import ReactEcharts from 'echarts-for-react';
 
 const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
@@ -79,6 +80,85 @@ const Dashboard = React.createClass({
             });
         });
 
+    },
+    getPieChartOtion() {
+        const option = {
+            backgroundColor: '#403F4C',
+            title: {
+                text: '',
+                subtext: '',
+                x: 'center',
+            },
+            tooltip: {
+                trigger: 'item',
+                formatter: '{a} <br />{b} : {c} ({d}%)',
+            },
+      
+            toolbox: {
+                show: false,
+                feature: {
+
+                    magicType: {
+                        show: true,
+                        type: ['pie', 'funnel'],
+                    },
+                    restore: { show: true },
+                    saveAsImage: { show: true },
+                },
+            },
+            color: ['#EAC435', '#345995', '#E40066', '#03CEA4', '#FB4D3D', '#FFF', '#FA7921', '#1B998B'],
+
+            calculable: true,
+            series: [
+                {
+                    name: 'Radius Mode',
+                    type: 'pie',
+                    radius: [20, 150],
+                    center: ['50%', 285],
+                    roseType: 'radius',
+                    width: '80%',
+                    max: 40,
+                    itemStyle: {
+                        normal: {
+                            label: {
+                                show: true,
+                            },
+                            labelLine: {
+                                show: true,
+                                smooth: 0.9,
+                                length: 5,
+                                length2: 10,
+                            },
+                            borderWidth: 0,
+                            shadowBlur: 30,
+                            shadowOffsetX: 0,
+                            shadowOffsetY: 10,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)',
+                        },
+                        emphasis: {
+                            label: {
+                                show: true,
+                            },
+                            labelLine: {
+                                show: true,
+                            },
+                        },
+                    },
+                    data: [
+                        { value: 10, name: 'Mercedes' },
+                        { value: 5, name: 'BMW' },
+                        { value: 15, name: 'Audi' },
+                        { value: 25, name: 'Volvo' },
+                        { value: 20, name: 'Subaru' },
+                        { value: 35, name: 'Ford' },
+                        { value: 30, name: 'Reanult' },
+                        { value: 40, name: 'Toyota' },
+                    ],
+                }
+
+            ],
+        };
+        return option;
     },
 
     onChange(e) {
@@ -171,13 +251,20 @@ const Dashboard = React.createClass({
                             <ChartComposed data={this.state.graphData} loading={this.state.loading} />
                         </Card>
                     </Col>
-                    <Col lg={18} md={24}>
+                    <Col lg={12} md={24}>
                         <Card
                             bordered={false}
                             bodyStyle={{
 
                             }}>
-                   
+                            <ReactEcharts
+                                option={this.getPieChartOtion()}
+                                style={{
+                                    height: '450px',
+                                    width: '100%',
+                                }}
+                                className="react_for_echarts"
+                            />
                         </Card>
                     </Col>
                 </Row>
