5588eb09e5dbb6694c1f293867c54cccc25ea430
ASHISH <ashish.anand619@gmail.com>
2018-03-01 14:51:39 +0530

    fixed font isuues

--- src/components/dashboard/verticalBarChart.js
+++ src/components/dashboard/verticalBarChart.js
@@ -7,7 +7,41 @@ function VerticalBarChart(props) {
   if (props.loading) { return null; }
   else {
     const data = props.data;
+    const NotAxisTickButLabel = props => {
 
+
+      return <text height={props.height} fontSize="10px" fill={props.fill} textAnchor={props.textAnchor} width={props.width} x={props.x} y={props.y}>
+        {
+          <tspan y={props.y + 20}>
+            {props.payload.value}
+          </tspan>
+        }
+      </text>
+
+
+    }
+
+    const NotAxisTickButLabely = props => {
+if(props.payload.value.length > 10)
+{
+  var values = props.payload.value.split(" ");
+      return <text height={props.height} fontSize="10px" fill={props.fill} textAnchor={props.textAnchor} width={props.width} x={props.x} y={props.y}>
+        {values.map((val, i) => (<tspan y={props.y  +i*10} x={props.x}>
+          {val}
+        </tspan>))
+
+        }
+      </text>
+}      
+   return <text height={props.height} fontSize="10px" fill={props.fill} textAnchor={props.textAnchor} width={props.width} x={props.x} y={props.y}>
+   {
+     <tspan >
+       {props.payload.value}
+     </tspan>
+   }
+ </text>
+
+    }
     return (
       <div className={styles.sales}>
         <ResponsiveContainer minHeight={360}>
@@ -15,9 +49,9 @@ function VerticalBarChart(props) {
             data={data}
             layout="vertical"
           >
-            <XAxis type="number"  tickLine={false}  axisLine={{ stroke: color.borderBase, strokeWidth: 1 }}  padding={{ right: 10 }} />
-            <YAxis type="category" dataKey="name" axisLine={{ stroke: color.borderBase, strokeWidth: 1 }}  tickLine={false} />
-            <CartesianGrid  stroke={color.borderBase} strokeDasharray='3 3' />
+            <XAxis type="number" tick={<NotAxisTickButLabel angle={0} />} tickLine={false} axisLine={{ stroke: color.borderBase, strokeWidth: 1 }} padding={{ right: 10 }} />
+            <YAxis type="category" tick={<NotAxisTickButLabely angle={0} />} dataKey="name" axisLine={{ stroke: color.borderBase, strokeWidth: 1 }} tickLine={false} />
+            <CartesianGrid stroke={color.borderBase} strokeDasharray='3 3' />
             <Tooltip />
             <Bar dataKey="visitCount" barSize={25} fill={color.blue} />
           </BarChart>
